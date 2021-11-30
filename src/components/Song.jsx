6e194e733d08d231cbabe7bbc1a93cdb1dffd413
Arafat <arafat97alim@gmail.com>
2021-12-01 01:12:45 +0530

    updated song component for fun.

--- src/components/Song.jsx
+++ src/components/Song.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Song = () => {
   return (
     <div className="song-container">
-      <h1>Picture</h1>
-      <h1>Song Name</h1>
-      <h1>Song Artists</h1>
+      <h1>Picture - 👸</h1>
+      <h1>Song Name: Afreen Afreen </h1>
+      <h1>Song Artists: Afreen Rahman</h1>
     </div>
   );
 };
