2d9a605e1af9a534a6f181e068d70ded90592870
Arafat <arafat97alim@gmail.com>
2021-12-01 23:16:33 +0530

    update song time.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faAngleLeft,
@@ -9,6 +9,7 @@ import {
 const Player = ({ currentSong, setIsPlaying, isPlaying }) => {
   // use Ref
   const audioRef = useRef(null);
+  // event handler
   const playSongHandler = () => {
     if (isPlaying) {
       audioRef.current.pause();
@@ -18,12 +19,27 @@ const Player = ({ currentSong, setIsPlaying, isPlaying }) => {
       setIsPlaying(!isPlaying);
     }
   };
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({ ...songInfo, currentTime: current, duration });
+  };
+  const getTime = (time) => {
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  };
+  // state
+  const [songInfo, setSongInfo] = useState({
+    currentTime: null,
+    duration: null,
+  });
   return (
     <div className="player">
       <div className="time-control">
-        <p>Start Time </p>
+        <p>{getTime(songInfo.currentTime)}</p>
         <input type="range" />
-        <p>End Time</p>
+        <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-control">
         <FontAwesomeIcon size="2x" icon={faAngleLeft} className="play" />
@@ -34,7 +50,12 @@ const Player = ({ currentSong, setIsPlaying, isPlaying }) => {
           className="play"
         />
         <FontAwesomeIcon size="2x" icon={faAngleRight} className="play" />
-        <audio ref={audioRef} src={currentSong.audio} />
+        <audio
+          ref={audioRef}
+          src={currentSong.audio}
+          onTimeUpdate={timeUpdateHandler}
+          onLoadedMetadata={timeUpdateHandler}
+        />
       </div>
     </div>
   );
