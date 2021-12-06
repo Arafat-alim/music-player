faefdb9e59b1a6d5cc88756075b0dd6fc05cbb7e
Arafat <arafat97alim@gmail.com>
2021-12-06 21:47:01 +0530

    major update - Fixed thumnail and added more songs.

--- src/data.js
+++ src/data.js
@@ -2,35 +2,41 @@ import { v4 as uuidv4 } from "uuid";
 function chillHop() {
   return [
     {
-      name: "Gone Forever",
-      cover: "https://i.scdn.co/image/ab67616d0000b273445b173eae8658091dad94ee",
-      artist: "Aso, Middle School, Aviino",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=24700",
-      color: ["#ED967B", "#2E8B78"],
+      name: "Nazm Nazm",
+      cover:
+        "https://is5-ssl.mzstatic.com/image/thumb/Music114/v4/66/4a/65/664a6508-5c51-a114-5ed0-3e8ac53fa0c4/source/600x600bb.jpg",
+      artist: "Ayushmann Khurrana ",
+      audio:
+        "https://res.cloudinary.com/blazzingcdn/video/upload/v1638805635/Songs/Nazm_Nazm_Feat._Ayushmann_Khurrana_Bareilly_Ki_Barfi_320_Kbps_nwbsh8.mp3",
+      color: ["#A92F60", "#8DDCD7"],
       id: uuidv4(),
       active: true,
     },
     {
-      name: "Underwater",
-      cover: "https://i.scdn.co/image/ab67616d0000b27352a13df0ef3dc23ed243d524",
-      artist: "SwuM",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=24714",
-      color: ["#882A42", "#F8C840"],
+      name: "Dil Galti Kar Baitha hai (Reprise)",
+      cover:
+        "https://is3-ssl.mzstatic.com/image/thumb/Music125/v4/c7/83/bd/c783bd16-31bf-6980-72bc-3b82ad79b9f9/source/600x600bb.jpg",
+      artist: "JalRaj",
+      audio:
+        "https://res.cloudinary.com/blazzingcdn/video/upload/v1638806016/Songs/X2Download.com_-_Dil_Galti_Kar_Baitha_hai_Reprise_-_JalRaj_-_Latest_Hindi_Cover_2021_-REUPLOAD-_128_kbps_wmewm8.mp3",
+      color: ["#F7CBE2", "#D34156"],
       id: uuidv4(),
       active: false,
     },
     {
-      name: "Forecast",
-      cover: "https://i.scdn.co/image/ab67616d0000b2731616f80b434c0eea4e980c88",
-      artist: "Ezzy, jacuzzi jefferson",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=24689",
-      color: ["#392E4C", "#2E8B78"],
+      name: "Phir Miloge Na - Single",
+      cover:
+        "https://is4-ssl.mzstatic.com/image/thumb/Music126/v4/7f/1a/7e/7f1a7e96-226b-a140-43db-6718ab872d2b/source/600x600bb.jpg",
+      artist: "JalRaj",
+      audio:
+        "https://res.cloudinary.com/blazzingcdn/video/upload/v1638805239/Songs/X2Download.com_-_Phir_Miloge_Na_-_JalRaj_-_Official_Video_-_New_Original_Songs_2021_128_kbps_cl1m74.mp3",
+      color: ["#512522", "#192E43"],
       id: uuidv4(),
       active: false,
     },
     {
       name: "Chidiya Reprise",
-      cover: "https://i3.ytimg.com/vi/35uA5_Ul7b0/hqdefault.jpg",
+      cover: "https://i.scdn.co/image/ab67616d0000b273fafed6d05f8e2836e0a2a483",
       artist: "JalRaj",
       audio:
         "https://res.cloudinary.com/blazzingcdn/video/upload/v1638793780/Songs/X2Download.com_-_Chidiya_Reprise_-_JalRaj_-_Vilen_-_Latest_cover_2021_Hindi_128_kbps_jo8ctj.mp3",
@@ -39,8 +45,9 @@ function chillHop() {
       active: false,
     },
     {
-      name: "Yeh pyaar nahi toh kya hai",
-      cover: "https://i3.ytimg.com/vi/ihBkVnaczds/hqdefault.jpg",
+      name: "Yeh pyaar Nahi Toh Kya Hai",
+      cover:
+        "https://sgdccdnems03.cdnsrv.jio.com/c.saavncdn.com/663/Ye-Pyar-Nahi-to-Kya-Hai-Unplugged-Version--Hindi-2020-20200330092223-500x500.jpg",
       artist: "Rahul Jain",
       audio:
         "https://res.cloudinary.com/blazzingcdn/video/upload/v1638793306/Songs/X2Download.com_-_Yeh_Pyar_Nahi_To_Kya_Hai_-_Title_Song_-_Rahul_Jain_-_Full_Song_-_Sony_TV_Serial_320_kbps_r7a5az.mp3",
@@ -99,6 +106,17 @@ function chillHop() {
       audio:
         "https://res.cloudinary.com/blazzingcdn/video/upload/v1638791928/Songs/Title_Track_Chandigarh_Kare_Aashiqui_320_Kbps_iohq42.mp3",
       color: ["#9D06F3", "#F1068F"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "Yeh Tune Kya Kiya",
+      cover:
+        "https://is3-ssl.mzstatic.com/image/thumb/Music114/v4/73/69/1c/73691c70-8d52-312b-a6b8-6be8267613e2/source/600x600bb.jpg",
+      artist: "Jassi Sidhu, IP Singh",
+      audio:
+        "https://res.cloudinary.com/blazzingcdn/video/upload/v1638806403/Songs/Yeh_Tune_Kya_Kiya_Song_Once_upon_A_Time_In_Mumbaai_Dobara_-_Pritam_-_Akshay_Kumar_Sonakshi_Sinha_zji6uy.mp4",
+      color: ["#BB2129", "#100F15"],
       id: uuidv4(),
       active: false,
     },
