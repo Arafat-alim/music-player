49e7dd63d372326dfafe4437ccef1b7bcfaaf1ed
Arafat <arafat97alim@gmail.com>
2021-12-24 23:48:13 +0530

    added more songs.

--- src/data.js
+++ src/data.js
@@ -478,17 +478,20 @@ function chillHop() {
       active: false,
     },
     {
-      name: "Aa To Sahii",
-      cover: "https://i1.sndcdn.com/artworks-000242957876-7r7bjo-t500x500.jpg",
-      artist: "Meet Bros, Neha Kakkar",
-      audio: "https://pagalnew.com/download320/3647",
-      color: ["#05075A", "#763462"],
+      name: "Give Me Some Sunshine",
+      cover:
+        "https://c-cl.cdn.smule.com/rs-s80/arr/b5/5a/9acbca39-faa2-4205-a120-7dc4494db937_1024.jpg",
+      artist: "Suraj Jagan",
+      audio:
+        "https://pagalnew.com/mp3-songs/bollywood-mp3-songs/give-me-some-sunshine-128-kbps-sound.mp3",
+      color: ["#945A11", "#000000"],
       id: uuidv4(),
       active: false,
     },
     {
       name: "Aa To Sahii",
-      cover: "https://i1.sndcdn.com/artworks-000242957876-7r7bjo-t500x500.jpg",
+      cover:
+        "https://pagalnew.com/coverimages/behti-hawa-sa-tha-woh-500-500.jpg",
       artist: "Meet Bros, Neha Kakkar",
       audio: "https://pagalnew.com/download320/3647",
       color: ["#05075A", "#763462"],
